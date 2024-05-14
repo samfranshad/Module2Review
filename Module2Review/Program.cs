fd951f26b1169505ad09696cf9eb1914ae1cea58
Sam Shadwell <sshadwell978@gmail.com>
2024-05-14 14:38:06 -0400

    In main, lines 3-13, created students list. Made 4 instances of Student and added to students list

--- Module2Review/Program.cs
+++ Module2Review/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using Module2Review;
+
+var students = new List<Student>();
+
+var studentOne = new Student() { Name = "Megan", ID = 320 };
+var studentTwo = new Student() { Name = "Jimmy", ID = 644 };
+var studentThree = new Student() { Name = "Casey", ID = 164 };
+var studentFour = new Student() { Name = "Jacob", ID = 067 };
+
+students.Add(studentOne);
+students.Add(studentTwo);
+students.Add(studentThree);
+students.Add(studentFour);
+
+
+
+
